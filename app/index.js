441a730ae2cb12bbafb49e6c7eeb45f8795792c8
othman853 <anuar.yasser@gmail.com>
2017-02-17 18:58:39 -0200

    Examining a sample wercker api response

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const vpath = require('value-box/path');
+vpath(__dirname, ['/stubs']);
+
+const { wercker } = require('value-box');
+
+const shrinkRun =
+  r => ({ name: r.pipeline.name, result: r.result, status: r.status, progress: r.progress });
+
+const pipelinesByCommit =
+  hash => wercker.filter(run => run.commitHash === hash).map(shrinkRun);
+
+const hashes =
+  wercker.map(run => run.commitHash);
+
+const singleCommitPipelines =
+  hashes.map(hash => ({ commit: hash, stages: pipelinesByCommit(hash) }));
+
+console.log(JSON.stringify(singleCommitPipelines));
