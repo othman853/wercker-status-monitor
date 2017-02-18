da5766f320af3e9f766a1bb95ecab84e3ce68f2c
othman853 <anuar.yasser@gmail.com>
2017-02-18 20:36:33 -0200

    Setting up socket and views

--- app/index.js
+++ app/index.js
@@ -1,6 +1,25 @@
 require('value-box/path')(__dirname, ['/stubs']);
+const express = require('express');
+const socketIo = require('socket.io');
 const transform = require('./transform');
+const { createServer } = require('http');
 const { wercker } = require('value-box');
 
+const app = express();
 
-console.log(JSON.stringify(transform(wercker)));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/wercker', (req, res) => res.json(transform(wercker)));
+app.get('/', (req, res) => res.render('index.ejs'));
+
+const httpServer = createServer(app);
+const socketServer = socketIo(httpServer);
+
+httpServer.listen(3000, console.log.bind(console, 'Up'));
+
+socketServer.on('connect', (socket) => {
+  socket.emit('update', transform(wercker));
+  socket.on('refresh', socket.emit.bind(socket, 'update', transform(wercker)));
+});
