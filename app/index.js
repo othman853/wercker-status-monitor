d59578a0d75657bc0d24e55283f94128f62515df
othman853 <anuar.yasser@gmail.com>
2017-02-19 22:36:56 -0300

    Serving io client from server

--- app/index.js
+++ app/index.js
@@ -1,15 +1,22 @@
 require('value-box/path')(__dirname, ['/stubs']);
-const express = require('express');
-const socketIo = require('socket.io');
-const transform = require('./transform');
+const { readFileSync } = require('fs');
 const { createServer } = require('http');
 const { wercker } = require('value-box');
+const express = require('express');
+const socketIo = require('socket.io');
+const socketClientPath = require.resolve(
+  'socket.io-client/dist/socket.io.min.js'
+);
+const transform = require('./transform');
 
 const app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.get('/socket.io-client.js',
+  (req, res) => res.send(readFileSync(socketClientPath))
+);
 
 app.get('/wercker', (req, res) => res.json(transform(wercker)));
 app.get('/', (req, res) => res.render('index.ejs'));
