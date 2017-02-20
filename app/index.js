efa518e1120a4c8486b210549bf4984a0bbade53
othman853 <anuar.yasser@gmail.com>
2017-02-20 13:18:39 -0300

    Refactoing app setup

--- app/index.js
+++ app/index.js
@@ -1,32 +1,17 @@
-require('value-box/path')(__dirname, ['/stubs']);
-const { readFileSync } = require('fs');
-const { createServer } = require('http');
-const { wercker } = require('value-box');
 const express = require('express');
-const socketIo = require('socket.io');
-const socketClientPath = require.resolve(
-  'socket.io-client/dist/socket.io.min.js'
-);
+const statics = require('./statics');
+const routes = require('./routes');
+const createServers = require('./create-servers');
 const transform = require('./transform');
+const { wercker } = require('value-box');
 
-const app = express();
+const app = routes(statics(express()));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.get('/socket.io-client.js',
-  (req, res) => res.send(readFileSync(socketClientPath))
-);
+const { httpServer, socketServer } = createServers(app);
 
-app.get('/wercker', (req, res) => res.json(transform(wercker)));
-app.get('/', (req, res) => res.render('index.ejs'));
-
-const httpServer = createServer(app);
-const socketServer = socketIo(httpServer);
-
-httpServer.listen(3000, console.log.bind(console, 'Up'));
-
-socketServer.on('connect', (socket) => {
+socketServer.on('connect', socket => {
   socket.emit('update', transform(wercker));
   socket.on('refresh', socket.emit.bind(socket, 'update', transform(wercker)));
 });
+
+module.exports = httpServer;
