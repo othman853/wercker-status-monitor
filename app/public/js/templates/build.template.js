343e23bc31956cb81f95a1b885a875824110c3b5
othman853 <anuar.yasser@gmail.com>
2017-02-21 15:59:43 -0300

    Making things bigger

--- app/public/js/templates/build.template.js
+++ app/public/js/templates/build.template.js
@@ -5,7 +5,7 @@ const BuildTemplate = function(build) {
     const templateString = stage =>
     `<section class="build-stage ${stage.result}">
       <h3 class="build-stage-result">${stage.result}</h3>
-      <p>${stage.name}</p>
+      <p class="build-stage-name">${stage.name}</p>
     </section>`;
     return stages.map(templateString).join('');
   };
@@ -13,7 +13,6 @@ const BuildTemplate = function(build) {
   const templateString =
   `<section class="build-info" id="${build.commitHash}">
     <section class="build-commit-details">
-      <img src="https://www.gravatar.com/avatar/${build.user.avatar.gravatar}" alt="">
       <h1>${build.commitMessage}</h1>
     </section>
     <section class="build-stages">
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,6 @@
 const processor = require('./processors/wercker-workflow-processor.js');
 const { werckerWorkflow } = require('value-box');
 
-console.log(werckerWorkflow);
-
 module.exports = app => {
   app.get('/', (req, res) => res.render('index.ejs'));
   app.get('/workflow', (req, res) => res.json(processor(werckerWorkflow)));
