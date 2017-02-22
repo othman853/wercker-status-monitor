edc6de903a51cb310ba1917c750cc23859d6b9ad
othman853 <anuar.yasser@gmail.com>
2017-02-22 19:40:03 -0300

    Adding host detailsroute

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,25 @@
 const processor = require('./processors/wercker-workflow-processor.js');
 const { werckerWorkflow } = require('value-box');
+const os = require('os');
 
 module.exports = app => {
   app.get('/', (req, res) => res.render('index.ejs'));
   app.get('/workflow', (req, res) => res.json(processor(werckerWorkflow)));
   app.post('/push', (req, res) => res.sendStatus(200));
+  app.get('/host', (req, res) => {
+
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+    return res.json({ ip: addresses[0], port: 3000 });
+  });
+
   return app;
 }
